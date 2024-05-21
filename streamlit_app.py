767002bee15abc2426d9bb76260e89429392dc90
mperelli <101881175+matiasperelli@users.noreply.github.com>
2024-05-21 17:53:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,30 @@
 import streamlit as st
 
-st.title("😎")
+st.title("HOLA")
+
+if "messages" not in st.session_state:
+    st.session_state.messages =[]
+if "first_message" not in st.session_state:
+    st.session_state.first_message = True
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+if st.session_state.first_message:
+    with st.chat_message("assistant"):
+        st.markdown("Hola,soy Pharmagenio, asistente farmaceutico virtual. ¿Dime en que te puedo ayudar? ")
+        st.session_state.messages.append({"role":"assistand", "content": "Hola,soy Pharmagenio, asistente farmaceutico virtual. ¿Dime en que te puedo ayudar?"})
+        st.session_state.firs_message = False
+
+
+if prompt := st.chat_input("¿como puedo ayudarte?"):
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content" : prompt})
+
+    with st.chat_message("assistant"):
+        st.markdown(prompt)
+
+    st.session_state.messages.append({"role": "assistant", "content": prompt})
